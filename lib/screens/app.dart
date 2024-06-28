d5d7343f9e78796323c4f0435ba33cb0e10ec9e0
Trần Đại Đăng <dangvipro3@gmail.com>
2024-06-28 23:52:24 +0700

    authen with google

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/services.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,14 +31,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() async {
+  String tokenFCM = "";
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  String name = "";
+  String email = "";
+  String image = "";
+
+  void getTokenFCM() async {
     String? token = await FirebaseMessaging.instance.getToken();
-    print(token);
     setState(() {
-     
-      _counter++;
+      tokenFCM = token ?? "";
     });
+  }
+
+  Future<void> copyToken() async {
+    await Clipboard.setData(ClipboardData(text: tokenFCM));
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      var googleInfo =  await _googleSignIn.signIn();
+      if (googleInfo != null) {
+        setState(() {
+          name = googleInfo.displayName ?? "";
+          email = googleInfo.email;
+          image = googleInfo.photoUrl ?? "";
+        });
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> _handleSignOut() async {
+    try {
+      await _googleSignIn.signOut();
+      setState(() {
+        name = "";
+        email = "";
+        image = "";
+      });
+    } catch (error) {
+      print(error);
+    }
   }
 
   @override
@@ -42,33 +81,109 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  
-
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal:  10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Tên: $name'),
+              Text('Email: $email'),
+              if (image.isNotEmpty)
+                Image.network(image, width: size.width * .3, height: size.width * .3,),
+              InkWell(
+                onTap:(){
+                  getTokenFCM();
+                },
+                splashColor: Colors.blue,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical:  10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(width: 1, color: Colors.black),
+                  ),
+                  width: size.width,
+                  height: 40,
+                  child: const Text(
+                    'Get token FCM',
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Token FCM: $tokenFCM',
+                    ),
+                  ),
+                  InkWell(
+                    onTap:(){
+                      copyToken();
+                    },
+                    splashColor: Colors.blue,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(width: 1, color: Colors.black),
+                      ),
+                      child: const Text(
+                        'Copy',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              InkWell(
+                onTap:(){
+                  _handleSignIn();
+                },
+                splashColor: Colors.blue,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical:  10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(width: 1, color: Colors.black),
+                  ),
+                  width: size.width,
+                  height: 40,
+                  child: const Text(
+                    'Login with google',
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap:(){
+                  _handleSignOut();
+                },
+                splashColor: Colors.blue,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical:  10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(width: 1, color: Colors.black),
+                  ),
+                  width: size.width,
+                  height: 40,
+                  child: const Text(
+                    'Sign out google',
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
